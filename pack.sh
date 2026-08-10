f877648d7e1ffe1be93380c5c2ef3219522f12d1
mafu <mafusukee@gmail.com>
2026-08-10 16:17:17 +0900

    feat(repo): add Sileo native depictions
    Add screenshots, concise product details, purchase links, and dated changelog tabs for Myrtle, K2geIsland, K2gecamen, and K2ge3Air. Refresh website copy to match current released features.

--- pack.sh
+++ pack.sh
@@ -1,3 +1,27 @@
-apt-ftparchive packages ./debs > Packages
+#!/bin/sh
+
+set -eu
+
+packages_tmp=$(mktemp)
+trap 'rm -f "$packages_tmp"' EXIT
+
+apt-ftparchive packages ./debs > "$packages_tmp"
+
+awk '
+{
+    print
+
+    if ($0 == "Package: com.m4fn3.myrtle") {
+        print "SileoDepiction: https://m4fn3.github.io/repo/web/depictions/Myrtle.json"
+    } else if ($0 == "Package: com.m4fn3.k2geisland") {
+        print "SileoDepiction: https://m4fn3.github.io/repo/web/depictions/K2geIsland.json"
+    } else if ($0 == "Package: com.m4fn3.k2gecamen") {
+        print "SileoDepiction: https://m4fn3.github.io/repo/web/depictions/K2gecamen.json"
+    } else if ($0 == "Package: com.m4fn3.k2ge3air") {
+        print "SileoDepiction: https://m4fn3.github.io/repo/web/depictions/K2ge3Air.json"
+    }
+}
+' "$packages_tmp" > Packages
+
 bzip2 -kf Packages
 gzip -c Packages > Packages.gz
